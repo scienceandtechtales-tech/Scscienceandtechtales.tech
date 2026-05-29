8b675e0b73d934a6585044da7171ebf4be2795d5
scienceandtechtales-tech <scienceandtechtales@gmail.com>
2026-05-29 08:50:23 +0100

    Create supabase.js

---
+++ public/src/src/lib/supabase.js
@@ -0,0 +1,4 @@
+import { createClient } from '@supabase/supabase-js'
+
+const SUPABASE_URL = 'https://emjfzitshisrnhquvjis.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVtamZ6aXRzaGlzcm5ocXV2amlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk5MTI2MjcsImV4cCI6MjA5NTQ4ODYyN30.cmS_UeDri
